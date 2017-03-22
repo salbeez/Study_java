fd5b562cb3bdef6698346ac579f3d636b7316579
ksota <ksota@ksota-PC>
2017-03-22 19:32:44 +0900

    찐짜 17/03/22끝

--- JavaApp5_weak/src/com/kosta/j0322/ObjectWriteTest.java
+++ JavaApp5_weak/src/com/kosta/j0322/ObjectWriteTest.java
@@ -19,22 +19,31 @@ public class ObjectWriteTest {
 					new ObjectOutputStream(fos);
 			
 			People  p =new People("홍길동", 12, "도적");
+			People  p2 =new People("길라임", 13, "스턴트맨");
+			People  p3 =new People("망나뇽", 14, "포켓몬");
+			People  p4 =new People("지영준", 27, "환자");
 			oos.writeObject(p);
+			oos.writeObject(p2);
+			oos.writeObject(p3);
+			oos.writeObject(p4);
 			oos.close();
 			System.out.println("객체쓰기 성공");
 //			==========================================
 			File file = new File("a.ser");//파일 정보 가져오고
-			FileInputStream fis = new FileInputStream(file);// 파일을 인풋 스트림으로 바꿔주고
+			FileInputStream fis = new FileInputStream(file);// 파일에 있는 정보를 가져오고
 			ObjectInputStream ois = new ObjectInputStream(fis);//오브젝트 형태로 가져온다
 			
-			Object ob;
+			int count=0;
 			System.out.println(ois.available());
-			while( (ob=ois.readObject()) != null){
-				People p_read = (People) ob;
+			while((count=fis.available()) >0){
+				System.out.println("\t\tcount : "+count);
+				People p_read = (People) ois.readObject();
 				System.out.println("이름 : "+p_read.getName());
 				System.out.println("나이 : "+p_read.getAge());
 				System.out.println("직업 : "+p_read.getJob());
 			}
+			
+			
 			
 			
 			fis.close();
@@ -47,3 +56,4 @@ public class ObjectWriteTest {
 		}
 	}
 }
+//	http://stackoverflow.com/questions/2626163/java-fileinputstream-objectinputstream-reaches-end-of-file-eof
